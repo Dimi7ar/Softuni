68b704677db6fa58aa210fe6d43ce4019e854d8e
Dimitar Dimitrov <111603656+Dimi7ar@users.noreply.github.com>
2024-01-09 19:21:21 +0200

    Create simple_calculator.java

---
+++ Java_Advanced/Stacks_and_Queues_Lab/simple_calculator.java
@@ -0,0 +1,27 @@
+import java.io.Console;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] inputArray = input.split("\\s");
+        ArrayDeque<String> stack = new ArrayDeque<String>();
+        for (Integer i = 0; i < inputArray.length; i++) {
+            stack.add(inputArray[i]);
+        }
+        Integer result = Integer.parseInt(stack.pop());
+        while (!stack.isEmpty()) {
+            if (stack.pop().equals("+")) {
+                result += Integer.parseInt(stack.pop());
+            } else {
+                result -= Integer.parseInt(stack.pop());
+            }
+        }
+        System.out.println(result);
+        scanner.close();
+    }
+}
