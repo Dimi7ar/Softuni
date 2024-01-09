bd6035a53d3054593b23d98a4cfd27c4ec62973c
Dimitar Dimitrov <111603656+Dimi7ar@users.noreply.github.com>
2024-01-09 19:20:35 +0200

    Create browser_history.java

---
+++ Java_Advanced/Stacks_and_Queues_Lab/browser_history.java
@@ -0,0 +1,30 @@
+import java.io.Console;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        ArrayDeque<String> stack = new ArrayDeque<String>();
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+                if (stack.size() <= 1) {
+                    System.out.println("no previous URLs");
+                } else {
+                    stack.pop();
+                    System.out.println(stack.peek());
+                }
+            } else {
+                stack.push(input);
+                System.out.println(stack.peek());
+            }
+
+            input = scanner.nextLine();
+        }
+        scanner.close();
+    }
+}
