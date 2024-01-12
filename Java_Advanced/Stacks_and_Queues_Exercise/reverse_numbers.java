b3311cd168879d3456d5afbbb73849c736e9b44d
Dimi7ar <dimitar.dtrv@gmail.com>
2024-01-12 20:16:52 +0200

    Update reverse_numbers.java

--- Java_Advanced/Stacks_and_Queues_Exercise/reverse_numbers.java
+++ Java_Advanced/Stacks_and_Queues_Exercise/reverse_numbers.java
@@ -1,17 +1,14 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class reverse_numbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().split("\\s");
-
-        ArrayDeque<String> deque = new ArrayDeque<String>();
+        ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
+        Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).forEach(deque::push);
         String result = "";
 
-        for (int i = 0; i < input.length; i++) {
-            deque.push(input[i]);
-        }
         while (!deque.isEmpty()) {
             result = result + " " + deque.pop();
         }
