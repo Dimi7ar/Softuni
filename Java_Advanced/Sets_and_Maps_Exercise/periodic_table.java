b22ff37f7ec596f36a7f79f782efedcada007b09
Dimi7ar <dimitar.dtrv@gmail.com>
2024-01-18 19:52:21 +0200

    Create periodic_table.java

---
+++ Java_Advanced/Sets_and_Maps_Exercise/periodic_table.java
@@ -0,0 +1,17 @@
+package Sets_and_Maps_Exercise;
+
+import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class periodic_table {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        TreeSet<String> set = new TreeSet<>();
+        for (int i = 0; i < n; i++) 
+            Arrays.stream(scanner.nextLine().split("\\s+")).forEach(set::add);
+        Arrays.stream(set.toArray()).forEach(elem -> System.out.printf("%s ", elem));
+        scanner.close();
+    }
+}
