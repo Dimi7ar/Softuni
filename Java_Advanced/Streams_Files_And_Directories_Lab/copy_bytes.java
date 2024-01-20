b0bb74307e317e799b5a7d78c320830a062f29fb
Dimi7ar <dimitar.dtrv@gmail.com>
2024-01-20 18:04:34 +0200

    Create copy_bytes.java

---
+++ Java_Advanced/Streams_Files_And_Directories_Lab/copy_bytes.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+
+public class copy_bytes {
+    public static void main(String[] args) throws IOException {
+        String root = System.getProperty("user.dir");
+
+        String inputPath = root
+                + "\\Streams_Files_And_Directories_Lab\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String outputPath = root
+                + "\\Streams_Files_And_Directories_Lab\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\copyBytes.txt";
+
+        FileInputStream fileInputStream = new FileInputStream(inputPath);
+        FileOutputStream fileOutputStream = new FileOutputStream(outputPath);
+
+        int oneByte = fileInputStream.read();
+        while (oneByte >= 0) {
+            String currAscii = String.valueOf(oneByte);
+            if (!(oneByte == 10 || oneByte == 32))
+                for (int i = 0; i < currAscii.length(); i++)
+                    fileOutputStream.write(currAscii.charAt(i));
+            else
+                fileOutputStream.write(oneByte);
+
+            oneByte = fileInputStream.read();
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
