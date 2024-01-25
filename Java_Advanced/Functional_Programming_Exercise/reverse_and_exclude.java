440c0ce74ce34040e87f2623dbe11b8731d09d3d
Dimi7ar <dimitar.dtrv@gmail.com>
2024-01-25 20:07:08 +0200

    Create reverse_and_exclude.java

---
+++ Java_Advanced/Functional_Programming_Exercise/reverse_and_exclude.java
@@ -0,0 +1,21 @@
+package Functional_Programming_Exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class reverse_and_exclude {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> listInput = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt)
+                .collect(Collectors.toList());
+        int n = Integer.parseInt(scanner.nextLine());
+        Predicate<Integer> isDivisible = number -> number % n == 0;
+        Collections.reverse(listInput);
+        listInput.removeIf(isDivisible);
+        listInput.stream().forEach(e -> System.out.print(e + " "));
+    }
+}
