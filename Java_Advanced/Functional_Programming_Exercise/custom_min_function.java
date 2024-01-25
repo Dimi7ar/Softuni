0b900dd2f94c3689a110fade01afae187e6db952
Dimi7ar <dimitar.dtrv@gmail.com>
2024-01-25 19:50:21 +0200

    Create custom_min_function.java

---
+++ Java_Advanced/Functional_Programming_Exercise/custom_min_function.java
@@ -0,0 +1,15 @@
+package Functional_Programming_Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class custom_min_function {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Function<Integer[], Integer> func = n -> Arrays.stream(n).min(Integer :: compareTo).get();
+        Integer[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).toArray(Integer[]::new);
+        System.out.println(func.apply(numbers));
+    }   
+}
