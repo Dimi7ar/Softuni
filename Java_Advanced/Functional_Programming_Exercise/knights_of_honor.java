6584386bc17d99299a916493c8fb0fdb31446a4a
Dimi7ar <dimitar.dtrv@gmail.com>
2024-01-25 19:40:17 +0200

    Create knights_of_honor.java

---
+++ Java_Advanced/Functional_Programming_Exercise/knights_of_honor.java
@@ -0,0 +1,13 @@
+package Functional_Programming_Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class knights_of_honor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Consumer<String> printName = n -> System.out.println("Sir " + n);
+        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(printName);
+    }   
+}
